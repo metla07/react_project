5e1cf1786e59c29891e2195b852d2733f80ba450
Andrey.A <metla07@bk.ru>
2022-06-28 17:33:03 +0300

    edits

--- src/componentList/List.jsx
+++ src/componentList/List.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import ChatIcon from '@mui/icons-material/Chat';
 import './List.css'
 
-const List = ({name, id}) => {
+const List = ({name}) => {
     return (
            <div className={'chatItem'}>
                <ChatIcon/>
                <h3>{name}</h3>
-               <span>{id}</span>
            </div>
     );
 }
